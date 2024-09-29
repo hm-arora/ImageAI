03445a8c8b5c1e621c626d4f5d1da9f44532026c
hm-arora <himanshuarora3097@gmail.com>
2024-09-30 01:21:30 +0530

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { cn, constructMetadata } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import { Provider } from "./provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -45,6 +46,7 @@ export default function RootLayout({
         >
           {/* <Navbar /> */}
           {children}
+          <Analytics />
           <Toaster />
         </body>
       </html>
